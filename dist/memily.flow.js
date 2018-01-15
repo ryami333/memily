e334f1493b9b334fd56c5a84f059cd5db8f9b11d
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-01-16 11:15:53 +1300

    Ignore dist

--- dist/memily.flow.js
+++
@@ -1,6 +0,0 @@
-// @flow
-
-declare export default function<FnArgs: Array<any>, FnReturn>(fn: (...args: FnArgs) => FnReturn, options?: $Exact<{cacheKey?: <FnArgs>(..._: Args) => string | number, maxAge?: number}>): (..._: FnArgs) => FnReturn;
-
-declare export function flush(): void;
-
